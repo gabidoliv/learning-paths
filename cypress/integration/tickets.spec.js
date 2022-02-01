adcbf9b35d08873f2a11897cccd6f7984d60e6b4
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2022-02-01 13:42:14 -0300

    Select element

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,13 +1,20 @@
 describe("Tickets", () => {
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
-    it.only("fills all the text input fields", () =>{
+
+    it("fills all the text input fields", () => {
         const firstName = "Gabriela"
         const lastName = "Oliveira"
+
         cy.get("#first-name").type(firstName);
         cy.get("#last-name").type(lastName);
         cy.get("#email").type("gabriela.oliveira@venturus.org.br");
         cy.get("#requests").type("Vegan");
-        cy.get("#signature").type(firstName+' '+lastName);
+        cy.get("#signature").type(firstName + ' ' + lastName);
     });
-    it ("has 'TICKETBOX` header's heading", () => {});
-});
+
+    it.only("select two tickets", () => {    // only -> lib do mocha para unit tests, será feito apenas o teste marcado
+        cy.get("#ticket-quantity").select("2");
+    });
+    
+    it("has 'TICKETBOX` header's heading", () => {});
+})
