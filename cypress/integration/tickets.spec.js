4dbac88fa06f8749de8db03b7245389ca10490c6
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2022-02-01 16:28:08 -0300

    Assertions

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -7,7 +7,7 @@ describe("Tickets", () => {
 
         cy.get("#first-name").type(firstName);
         cy.get("#last-name").type(lastName);
-        cy.get("#email").type("gabriela.oliveira@venturus.org.br");
+        cy.get("#email").type("gabi.oliv16@gmail.com");
         cy.get("#requests").type("Vegan");
         cy.get("#signature").type(firstName + ' ' + lastName);
     });
@@ -24,11 +24,30 @@ describe("Tickets", () => {
         cy.get("#social-media").check();
     });
 
-    it.only("selects 'friend', and 'publication', then unchek 'friend'", () =>{
+    it("selects 'friend', and 'publication', then unchek 'friend'", () =>{
         cy.get("#friend").check();
         cy.get("#publication").check();
         cy.get("#friend").uncheck();
     });
 
-    it("has 'TICKETBOX` header's heading", () => {});
+    it("has 'TICKETBOX` header's heading", () => {
+        cy.get("header h1").should("contain", "TICKETBOX");
+    });
+
+    it.only("alerts on invalid email", () => {
+        cy.get("#email")
+          .as("email")
+          .type("gabi.oliv16-gmail.com");
+        
+        cy.get("#email.invalid").should("exist"); 
+
+        //cy.get("#email.invalid").as("invalidEmail").should("exist"); // Salva o elemento no momento que cria o alias e o teste não passa
+        
+        cy.get("@email")
+          .clear()
+          .type("gabi.oliv16@gmail.com");
+
+        //cy.get("@invalidEmail").should("not.exist");}); 
+        cy.get("#email.invalid").should("not.exist");
+    })
 });
