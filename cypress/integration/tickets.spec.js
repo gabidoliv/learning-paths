9537e35dd9cfa6d55ed53ae322837b4892766d93
Gabriela <vntgave@Gabriela-2.local>
2022-02-10 09:22:13 -0300

    All tests running

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -76,7 +76,7 @@ describe("Tickets", () => {
         cy.get("@submitButton").should("be.disabled");
     });
 
-    it.only("fills mandatory fields using support command", () => {
+    it("fills mandatory fields using support command", () => {
         const customer = {
             firstName: "João",
             lastName: "Silva",
