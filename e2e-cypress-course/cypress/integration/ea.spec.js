d7913ee1db7278d2453c3c061e1028fef974b3f9
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2022-03-22 16:33:35 -0300

    Updating learning paths

---
+++ e2e-cypress-course/cypress/integration/ea.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe("Testing of EA App", () => {
+
+    it("Login application", () => {
+        cy.visit("http://eaapp.somee.com/");
+
+        cy.contains("Login").click();
+
+        cy.url().should("include","Account/Login");
+
+        cy.get("#UserName").type("admin");
+
+        cy.get("#Password").type("password");
+
+        cy.get(".btn").click;
+            
+    })
+
+
+
+})
