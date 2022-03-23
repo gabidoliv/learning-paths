114c0a96d24d5b7b9cc07673fd6bc126f9ac003e
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2022-03-23 16:23:34 -0300

    cypress - ui elements, tables and variables

--- e2e-cypress-course/cypress/integration/ea.spec.js
+++ e2e-cypress-course/cypress/integration/ea.spec.js
@@ -5,16 +5,27 @@ describe("Testing of EA App", () => {
     it("Login application", () => {
         cy.visit("http://eaapp.somee.com/");
 
-        cy.contains("Login").click();
+        //cy.contains("Login").click();
 
-        cy.url().should("include","Account/Login");
+        cy.get("#loginLink").then(($link) => {
+            const linkText = $link.text();
+            expect(linkText).is.eql('Login');
+        }).click();
+
+        cy.url().should("include", "Account/Login");
 
         cy.get("#UserName").type("admin");
 
         cy.get("#Password").type("password");
 
-        cy.get(".btn").click;
-            
+        cy.get(".btn").click({ force: true });
+
+        //Click Employee List
+        cy.contains("Employee List").click();
+
+        //Table
+        cy.get('.table').find('tr').contains('Prashanth').parent().contains('Benefits').click();
+
     })
 
 
