1541a78d74d853f56c974fe0be4b1e75141b4756
Gabriela <85120294+gabidoliv@users.noreply.github.com>
2022-04-18 13:58:55 -0300

    update e2e

--- e2e-cypress-course/cypress/integration/ea.spec.js
+++ e2e-cypress-course/cypress/integration/ea.spec.js
@@ -2,15 +2,40 @@
 
 describe("Testing of EA App", () => {
 
+    it.only(" Testing EA Site for assertion", () => {
+        cy.visit("http://www.executeautomation.com/site");
+
+        //Implicit wait
+        // cy.get("jump to slide 2'",{timeout:60000})
+        // .should('have.class','ls-nav-active');
+
+        //Explicit wait - better readbility - in this example the animation is slow so it's recommended to use explicit waiting mecanism
+        cy.get("[aria-label='jump to slide 2']",{timeout:60000}).should(($x) => {
+            expect($x).to.have.class('ls-nav-active');
+        })
+
+        //cy.get("[aria-label='jump to slide 2']",{timeout:60000}).should('have.class', 'ls-nav-active');
+
+    })
     it("Login application", () => {
         cy.visit("http://eaapp.somee.com/");
 
+
         //cy.contains("Login").click();
 
-        cy.get("#loginLink").then(($link) => {
-            const linkText = $link.text();
-            expect(linkText).is.eql('Login');
-        }).click();
+        // Long way of working with Promise (Closure)
+        // cy.get("#loginLink").then(($link) => {
+        //     return $link.text();
+        // }).as("linkText");
+
+        // Shorthand wat of working with promise using invoke
+        cy.get("#loginLink").invoke('text').as("linkText");
+
+        cy.contains("Login").click();
+
+        cy.get("@linkText").then(($x) => {
+            expect($x).is.eql('Login');
+        })
 
         cy.url().should("include", "Account/Login");
 
@@ -23,8 +48,24 @@ describe("Testing of EA App", () => {
         //Click Employee List
         cy.contains("Employee List").click();
 
-        //Table
+        Table
         cy.get('.table').find('tr').contains('Prashanth').parent().contains('Benefits').click();
+
+        cy.get('.table').find('tr').as("rows");
+
+        cy.get("@rows").then(($row) => {
+
+            cy.wrap($row).click({ multiple: true });
+
+        })
+
+        //Verify the value from a property
+        cy.wrap({ name: 'Karthik' }).should('have.property', 'name').and('eq', 'Karthik');
+
+        //Using here - not the more useful or handy method to work with
+        // cy.get('.table').find('tr > td').then(($td) => {
+        //     cy.wrap($td).contains("John").invoke("wrap").parent().contains("Benefits").click();
+        // })
 
     })
 
